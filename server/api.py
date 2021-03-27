694f96b7090b11c2d421a912b6aedd454007790f
Konrad Walas <kwalas314@gmail.com>
2021-03-27 14:07:57 +0100

    Add json management to POST endpoints

--- server/api.py
+++ server/api.py
@@ -9,22 +9,46 @@ def hello():
 
 @apiv1.route('/auth/login', methods=['POST'])
 def login():
-    return "auth login"
+    json_data = request.get_json()
+    email = json_data['email']
+    password = json_data['password']
+
+    return f'email = {email}, password = {password}'
 
 @apiv1.route('/auth/register', methods=['POST'])
 def register():
-    return "auth register"
+    json_data = request.get_json()
+    email = json_data['email']
+    password = json_data['password']
+    first_name = json_data['firstName']
+    last_name = json_data['lastName']
+
+    return f'email = {email}, password = {password}, first_name = {first_name}, last_name = {last_name}'
 
 @apiv1.route('/help-offer', methods=['GET', 'POST'])
 def help_offer():
     if request.method == 'POST':
-        return "help-offer post"
+        json_data = request.get_json()
+        title = json_data['title']
+        description = json_data['description']
+        category = json_data['category']
+        tags = json_data['tags']
+
+        return f'title = {title}, description = {description}, category = {category}, tags = {tags}'
+
     if request.method == 'GET':
+
         return "help-offer GET"
 
 @apiv1.route('/help-request', methods=['GET', 'POST'])
 def help_request():
     if request.method == 'POST':
-        return "help-request post"
+        json_data = request.get_json()
+        title = json_data['title']
+        description = json_data['description']
+        category = json_data['category']
+        tags = json_data['tags']
+
+        return f'title = {title}, description = {description}, category = {category}, tags = {tags}'
     if request.method == 'GET':
         return "help-request GET"
