801f23d1c8e8fadf7b2fe12feed7bb9264e2aa0d
Konrad Walas <kwalas314@gmail.com>
2021-03-27 22:46:14 +0100

    Fix CORS

--- server/app.py
+++ server/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from resources import res 
 from auth import auth
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(res)
 app.register_blueprint(auth)
