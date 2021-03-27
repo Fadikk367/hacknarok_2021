2ec1d6ab0be0b2df66ec8a247f539e1df1f93330
Konrad Walas <kwalas314@gmail.com>
2021-03-27 13:34:18 +0100

    Add basic endopints with hello world

--- server/api.py
+++ server/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 apiv1 = Blueprint('api', __name__, url_prefix='/api/v1')
 
@@ -6,3 +6,25 @@ apiv1 = Blueprint('api', __name__, url_prefix='/api/v1')
 @apiv1.route('/')
 def hello():
     return "Hello World!"
+
+@apiv1.route('/auth/login', methods=['POST'])
+def login():
+    return "auth login"
+
+@apiv1.route('/auth/register', methods=['POST'])
+def register():
+    return "auth register"
+
+@apiv1.route('/help-offer', methods=['GET', 'POST'])
+def help_offer():
+    if request.method == 'POST':
+        return "help-offer post"
+    if request.method == 'GET':
+        return "help-offer GET"
+
+@apiv1.route('/help-request', methods=['GET', 'POST'])
+def help_request():
+    if request.method == 'POST':
+        return "help-request post"
+    if request.method == 'GET':
+        return "help-request GET"
