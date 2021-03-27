44eaee2ec03287dfcac1e44a1d96a86e4be12aed
mateuszGorczany <mateusz.g64@interia.pl>
2021-03-27 14:46:09 +0100

    utils fix

--- server/utils.py
+++ server/utils.py
@@ -1,18 +1,18 @@
-import environ
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 base = Path(__file__).parent
 ENV_PATH = str((base / ".env").resolve())
 
 
 def env():
-    envir = environ.Env(DEBUG=(bool, False))
-    environ.Env.read_env(env_file=ENV_PATH)
+    load_dotenv(ENV_PATH)
 
-    MONGO_USER = envir('MONGO_USER')
-    MONGO_PASS = envir('MONGO_PASS')
-    MONGO_HOST = envir('MONGO_HOST')
-    MONGO_OPT = envir('MONGO_OPT')
+    MONGO_USER = os.getenv('MONGO_USER')
+    MONGO_PASS = os.getenv('MONGO_PASS')
+    MONGO_HOST = os.getenv('MONGO_HOST')
+    MONGO_OPT = os.getenv('MONGO_OPT')
 
     return \
     'mongodb+srv://%s:%s@%s/%s/consulti' \
